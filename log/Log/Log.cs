bb84879258e679eca7a3a60afb36863941f85769
Александр Дрожкин <Santay2009@yandex.ru>
2020-09-13 12:04:49 +0300

    * [log] : Убран игнор с библиотеки по работе с логами

---
+++ log/Log/Log.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Extension;
+
+namespace Log
+{
+    /// <summary>
+    /// Класс дял формирования лога
+    /// </summary>
+    public static class Log
+    {
+        private static readonly string Path = Environment.CurrentDirectory + @"\Log";
+        private static readonly object Locker = new object();
+
+        public static void SetLog(string tag, string message)
+        {
+            lock (Locker)
+            {
+                if (!Directory.Exists(Path))
+                {
+                    Directory.CreateDirectory(Path);
+                }
+
+                using (StreamWriter streamWriter = new StreamWriter(Path + $@"\{DateTime.Now:ddMM_HH}.log", true))
+                {
+                    string messageLog = $"---{DateTime.Now:g}---{Environment.NewLine}"
+                                        + tag
+                                        + Environment.NewLine
+                                        + message
+                                        + Environment.NewLine
+                                        + new string('-', 30);
+                    streamWriter.WriteLine(messageLog);
+                }
+            }
+        }
+
+        public static void ClearLog()
+        {
+            DirectoryInfo folder = new DirectoryInfo(Path);
+            folder.GetFiles().ForEach(file => file.Delete());
+        }
+    }
+}
